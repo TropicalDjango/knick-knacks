3c89e6c3c1c515a6b5933f9a748957b56b02f537
Ahmed Refik <refik@ualberta.ca>
2023-05-11 20:36:12 -0600

    pynba can now display nba,nfl,nhl,mlb games and can show ranges of weeks

--- pynba/nba_games.py
+++ pynba/nba_games.py
@@ -4,88 +4,11 @@ import requests
 import lxml.html as lxml
 import datetime
 import readline
+import argparse
+from team_colors import *
 from subprocess import run
 from bs4 import BeautifulSoup
 from colorama import Fore, Style
-
-"""
-These are the ANSI color codes for the teams, I used
-https://teamcolorcodes.com/nba-team-color-codes/ for the RGB values I was
-going to have different colors for depending on away or home status, I haven't
-ruled it out entirely so these are still here, althrough they are the same
-
-the format for RGB ANSI is as follows
-
-bold          Foreground   Background
-|_____|     |____________|____________|
-\033[1m \033[38;2;{R:G:B};48;2;{R:G:B}m
-"""
-
-team_col_away = {
-    "Atlanta":          "\033[1m\033[38;2;253;185;39;48;2;200;16;46m",
-    "Boston":           "\033[1m\033[38;2;255;255;255;48;2;0;122;51m",
-    "Brooklyn":         "\033[1m\033[38;2;255;255;255;48;2;0;0;0m",
-    "Charlotte":        "\033[1m\033[38;2;255;255;255;48;2;29;17;96m",
-    "Chicago":          "\033[1m\033[38;2;255;255;255;48;2;206;17;65m",
-    "Cleveland":        "\033[1m\033[38;2;253;187;48;48;2;134;0;56m",
-    "Dallas":           "\033[1m\033[38;2;255;255;255;48;2;0;83;188m",
-    "Denver":           "\033[1m\033[38;2;255;198;39;48;2;13;34;64m",
-    "Detroit":          "\033[1m\033[38;2;237;23;76;48;2;0;45;98m",
-    "Golden State":     "\033[1m\033[38;2;255;199;44;48;2;29;66;138m",
-    "Houston":          "\033[1m\033[38;2;196;206;211;48;2;186;12;47m",
-    "Indiana":          "\033[1m\033[38;2;253;187;48;48;2;0;45;98m",
-    "Clippers":         "\033[1m\033[38;2;200;16;46;48;2;255;255;255m",
-    "Los Angeles":      "\033[1m\033[38;2;253;185;39;48;2;85;37;130m",
-    "Memphis":          "\033[1m\033[38;2;93;118;169;48;2;18;23;63m",
-    "Miami":            '\033[1m\033[38;2;255;255;255;48;2;152;0;46m',
-    "Milwaukee":        "\033[1m\033[38;2;240;235;210;48;2;0;71;27m",
-    "Minnesota":        "\033[1m\033[38;2;35;97;146;48;2;12;35;64m",
-    "New Orleans":      "\033[1m\033[38;2;180;151;90;48;2;0;22;65m",
-    "New York":         "\033[1m\033[38;2;245;132;38;48;2;0;107;182m",
-    "Oklahoma City":    "\033[1m\033[38;2;240;70;40;48;2;0;110;180m",
-    "Orlando":          "\033[1m\033[38;2;196;206;211;48;2;0;125;197m",
-    "Philadelphia":     "\033[1m\033[38;2;255;255;255;48;2;0;47;108m",
-    "Phoenix":          "\033[1m\033[38;2;229;95;32;48;2;29;17;96m",
-    "Portland":         "\033[1m\033[38;2;0;0;0;48;2;224;58;62m",
-    "Sacramento":       "\033[1m\033[38;2;255;255;255;48;2;91;43;130m",
-    "San Antonio":      "\033[1m\033[38;2;6;25;34;48;2;196;206;211m",
-    "Toronto":          "\033[1m\033[38;2;6;25;34;48;2;206;17;65m",
-    "Utah":             "\033[1m\033[38;2;249;160;27;48;2;0;43;92m",
-    "Washington":       "\033[1m\033[38;2;0;43;92;48;2;227;24;4m"
-}
-
-team_col_home = {
-    "Atlanta":          "\033[1m\033[38;2;253;185;39;48;2;200;16;46m",
-    "Boston":           "\033[1m\033[38;2;255;255;255;48;2;0;122;51m",
-    "Brooklyn":         "\033[1m\033[38;2;255;255;255;48;2;0;0;0m",
-    "Charlotte":        "\033[1m\033[38;2;255;255;255;48;2;29;17;96m",
-    "Chicago":          "\033[1m\033[38;2;255;255;255;48;2;206;17;65m",
-    "Cleveland":        "\033[1m\033[38;2;253;187;48;48;2;134;0;56m",
-    "Dallas":           "\033[1m\033[38;2;255;255;255;48;2;0;83;188m",
-    "Denver":           "\033[1m\033[38;2;255;198;39;48;2;13;34;64m",
-    "Detroit":          "\033[1m\033[38;2;237;23;76;48;2;0;45;98m",
-    "Golden State":     "\033[1m\033[38;2;255;199;44;48;2;29;66;138m",
-    "Houston":          "\033[1m\033[38;2;196;206;211;48;2;186;12;47m",
-    "Indiana":          "\033[1m\033[38;2;253;187;48;48;2;0;45;98m",
-    "Clippers":         "\033[1m\033[38;2;200;16;46;48;2;255;255;255m",
-    "Los Angeles":      "\033[1m\033[38;2;253;185;39;48;2;85;37;130m",
-    "Memphis":          "\033[1m\033[38;2;93;118;169;48;2;18;23;63m",
-    "Miami":            '\033[1m\033[38;2;255;255;255;48;2;152;0;46m',
-    "Milwaukee":        "\033[1m\033[38;2;240;235;210;48;2;0;71;27m",
-    "Minnesota":        "\033[1m\033[38;2;35;97;146;48;2;12;35;64m",
-    "New Orleans":      "\033[1m\033[38;2;180;151;90;48;2;0;22;65m",
-    "New York":         "\033[1m\033[38;2;245;132;38;48;2;0;107;182m",
-    "Oklahoma City":    "\033[1m\033[38;2;240;70;40;48;2;0;110;180m",
-    "Orlando":          "\033[1m\033[38;2;196;206;211;48;2;0;125;197m",
-    "Philadelphia":     "\033[1m\033[38;2;255;255;255;48;2;0;47;108m",
-    "Phoenix":          "\033[1m\033[38;2;229;95;32;48;2;29;17;96m",
-    "Portland":         "\033[1m\033[38;2;0;0;0;48;2;224;58;62m",
-    "Sacramento":       "\033[1m\033[38;2;255;255;255;48;2;91;43;130m",
-    "San Antonio":      "\033[1m\033[38;2;6;25;34;48;2;196;206;211m",
-    "Toronto":          "\033[1m\033[38;2;6;25;34;48;2;206;17;65m",
-    "Utah":             "\033[1m\033[38;2;249;160;27;48;2;0;43;92m",
-    "Washington":       "\033[1m\033[38;2;0;43;92;48;2;227;24;4m"
-}
 
 
 def team_schedule(url):
@@ -102,18 +25,23 @@ This is to print a single game line
 
 
 def print_game(away, home, game_time, tv_network):
-    away = team_col_away[away] + away.center(15) + Style.RESET_ALL
-    home = team_col_home[home] + home.center(15) + Style.RESET_ALL
+    # This causes teams not in team_col to be displayed as black on white
+    away = str(team_col.get(away) or team_col["Unknown"]) +\
+                                     away.center(15) + Style.RESET_ALL
+    home = str(team_col.get(home) or team_col["Unknown"]) +\
+                                    home.center(15) + Style.RESET_ALL
     if game_time == "LIVE":
         game_time = Fore.RED + "  ● LIVE  " + Style.RESET_ALL
-    elif len(game_time) > 8:
+    elif len(game_time) > 8 and game_time != "Postponed":
         game_time = Fore.GREEN + game_time.split(" ")[0] + "-" +\
                                      game_time.split(" ")[1] + Fore.RED +\
                                      game_time.split(" ")[2] + "-" +\
                                      game_time.split(" ")[3] + Style.RESET_ALL
         game_time = ''.join(game_time)
     else:
-        game_time = game_time.center(10) + Style.RESET_ALL
+        game_time = game_time.center(12) + Style.RESET_ALL
+
+    # This means tv_network is the series lead of a playoff game
     if len(tv_network) > 5:
         print("{}@{}:{}".format(away, home, game_time))
         tv_network = tv_network + Style.RESET_ALL
@@ -121,6 +49,46 @@ def print_game(away, home, game_time, tv_network):
     else:
         tv_network = tv_network.center(6) + Style.RESET_ALL
         print("{}@{}:{}:{}".format(away, home, game_time, tv_network))
+
+
+def get_day_table(single_game):
+    try:
+        away_team = single_game.find_class("events__col")[0].\
+                                find_class("AnchorLink")[1].text_content()
+    except IndexError:
+        away_team = single_game.find_class("events__col")[0].text_content()
+    try:
+        home_team = single_game.find_class("colspan__col")[0].\
+                                find_class("AnchorLink")[1].text_content()
+    except IndexError:
+        home_team = single_game.find_class("colspan__col")[0].\
+                                find_class("Table__Team")[0].text_content()
+    try:
+        if single_game.find_class("date__col") == []:
+            game_or_time = single_game.find_class("teams__col")[0].\
+                                    find_class("AnchorLink")[0].text_content()
+        else:
+            game_or_time = single_game.find_class("date__col")[0].\
+                                    find_class("AnchorLink")[0].text_content()
+    except IndexError:
+        game_or_time = "LIVE"
+    try:
+        tv_net = single_game.find_class("broadcast__col")[0].\
+                             find_class("network-name")[0].text_content()
+    except IndexError:
+        try:
+            tv_net = single_game.find_class("broadcast__col")[0]\
+                                     .find_class("")[0]
+            tv_net = lxml.tostring(tv_net).decode('ascii')
+            soup = BeautifulSoup(tv_net, 'lxml')
+            for img_tag in soup.find_all('img'):
+                tv_net = img_tag.get('alt')
+        except IndexError:
+            try:
+                tv_net = single_game.find_class("gameNote")[0].text_content()
+            except IndexError:
+                tv_net = ""
+    return [away_team, home_team, game_or_time, tv_net]
 
 
 """
@@ -132,64 +100,32 @@ The ESPN webpage is structured like this
                                                         -> <-colspan__col->
                                                         -> <--date__col--->
                                                         -> <broadcast__col>
-There is a different ResponsiveTable depending on wheater or the game finished
-or nor. events_col has the away team, colspan_col has the home team, date_col
-has the time, broadcast__col has the tv_network
+There is a different ResponsiveTable depending on wheater the game finished.
+events_col has the away team, colspan_col has the home team, date_col has the
+time, broadcast__col has the tv_network
     @param  url   the espn url to get the game details
 """
 
 
-def week_schedule(url):
-    run(["clear"])
+def get_week_schedule(url):
+    print(url)
     page = requests.get(url)
     tree = lxml.fromstring(page.text)
     elements = tree.find_class("ScheduleTables")
     for element in elements:
-        #print(lxml.tostring(element).decode('ascii'))
         day = element.find_class("Table__Title")
         if isinstance(day, list):
             day = day[0].text_content()
         else:
             day = day.text_content()
-        print('\n')
+        print('')
         print(day.center(54, "_"))
         for table in element.find_class("ResponsiveTable"):
             games = table.find_class("Table__TBODY")[0]
             for single_game in games.find_class("Table__TR--sm"):
-                away_team = single_game.find_class("events__col")[0].\
-                                        find_class("AnchorLink")[1].text_content()
-                home_team = single_game.find_class("colspan__col")[0].\
-                                        find_class("AnchorLink")[1].text_content()
-                try:
-                    if single_game.find_class("date__col") == []:
-                        game_or_time = single_game.find_class("teams__col")[0].\
-                                                find_class("AnchorLink")[0].text_content()
-                    else:
-                        game_or_time = single_game.find_class("date__col")[0].\
-                                                find_class("AnchorLink")[0].text_content()
-                except IndexError:
-                    game_or_time = "LIVE"
-                try:
-                    tv_net = single_game.find_class("broadcast__col")[0].\
-                                         find_class("network-name")[0].text_content()
-                except IndexError:
-                    try:
-                        tv_net = single_game.find_class("broadcast__col")[0].\
-                            find_class("")[0]
-                        tv_net = lxml.tostring(tv_net).decode('ascii')
-                        soup = BeautifulSoup(tv_net, 'lxml')
-                        for img_tag in soup.find_all('img'):
-                            tv_net = img_tag.get('alt')
-                    except IndexError:
-                        try:
-                            tv_net = single_game.find_class("gameNote")[0].\
-                               text_content()
-                        except IndexError:
-                            tv_net = ""
-                series_lead = single_game.find_class("events__col")[0].\
-                                          find_class("gameNote")[0].text_content()
-                print_game(away_team, home_team, game_or_time, tv_net)
-            print(''.center(54, '-'))
+                game_info = get_day_table(single_game)
+                print_game(game_info[0], game_info[1],
+                           game_info[2], game_info[3])
 
 
 """
@@ -197,34 +133,109 @@ This is just for testing
 """
 
 
-def test_teams(time, network):
-    for team in team_col_home:
-        print_game(team, team, time, network)
+def test_teams(team_col):
+    for team in team_col:
+        print_game(team, team, "LIVE", "ESPN")
     print('\n\n')
     exit(0)
 
 
+def get_selected_week(factor, base_url):
+    today = datetime.datetime.now()
+    today += datetime.timedelta(days=7*factor)
+    dt_string = today.strftime("%Y%m%d")
+    url = base_url + "/_/date/" + dt_string
+    print('\n' + 'Week {}'.format(factor).center(54, '-'))
+    get_week_schedule(url)
+
+
 if __name__ == "__main__":
-    base_url = "https://www.espn.com/nba/schedule"
     options = ["This Week", "Last Week", "Next Week", "Exit"]
-    while 1:
+    arguments = argparse.ArgumentParser(
+        description="This program can display the nba game schdule for a given week"
+    )
+
+    arguments.add_argument(
+        "-i",
+        "--interactive",
+        action="store_true",
+        help="enter interactive mode",
+    )
+
+    arguments.add_argument(
+        "-l",
+        "--league",
+        type=str,
+        nargs=1,
+        metavar=["('NBA', 'NHL', 'NFL')"],
+        default=["NBA"],
+        help="enter a sports league form the available options"
+    )
+
+    arguments.add_argument(
+        "-w",
+        "--week",
+        type=int,
+        nargs=1,
+        metavar=['week_number'],
+        default=[0],
+        help="enter a index of week you want to see from current"
+    )
+
+    arguments.add_argument(
+        "-r",
+        "--range",
+        type=int,
+        nargs=2,
+        metavar=['week1', 'week2'],
+        help="enter two relative week number and get all games between them"
+    )
+
+    arguments.add_argument(
+        "-t",
+        "--test",
+        action="store_true",
+        help="enter two relative week number and get all games between them"
+    )
+
+    flags = arguments.parse_args()
+    today = datetime.datetime.now()
+
+    match flags.league[0].upper():
+        case "NBA":
+            base_url = "https://www.espn.com/nba/schedule"
+            team_col = team_col_NBA
+        case "NFL":
+            base_url = "https://www.espn.com/nfl/schedule"
+            team_col = team_col_NFL
+        case "NHL":
+            base_url = "https://www.espn.com/nhl/schedule"
+            team_col = team_col_NHL
+        case "MLB":
+            base_url = "https://www.espn.com/mlb/schedule"
+            team_col = team_col_MLB
+
+    if flags.test:
+        test_teams(team_col)
+        exit(0)
+
+    while flags.interactive:
         print("Select an option from below".center(54, "_"))
         print("1:{}\n2:{}\n3:{}\n4:{}\n".format(options[0], options[1],
                                                 options[2], options[3]))
         user_input = int(input("-> "))
-        today = datetime.datetime.now()
+        run(["clear"])
         match user_input:
             case 1:
-                week_schedule(base_url)
+                get_selected_week(0, base_url)
             case 2:
-                today -= datetime.timedelta(days=7)
-                dt_string = today.strftime("%Y%m%d")
-                url = base_url + "/_/date/" + dt_string
-                week_schedule(url)
+                get_selected_week(-1, base_url)
             case 3:
-                today += datetime.timedelta(days=7)
-                dt_string = today.strftime("%Y%m%d")
-                url = base_url + "/_/date/" + dt_string
-                week_schedule(url)
+                get_selected_week(1, base_url)
             case 4:
                 exit(0)
+    if flags.range is not None:
+        for ii in range(flags.range[0], flags.range[1] + 1):
+            get_selected_week(ii, base_url)
+    else:
+        get_selected_week(flags.week[0], base_url)
